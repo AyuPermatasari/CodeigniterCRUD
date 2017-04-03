41dad9b6a2b0844d7da8f54e15986f33bc97fda7
AyuPermatasari <mayu.erizawa@gmail.com>
2017-04-03 11:37:03 +0700

    codeigniter CRUD

---
+++ application/views/tambah_pegawai_sukses.php
@@ -0,0 +1,9 @@
+<?php 
+
+	echo "Sukses Simpan Data";
+	echo "</br>";
+	echo "Masukkan Data Lagi ?";
+	echo "</br>";
+	echo anchor('pegawai/create', 'Ya    ');
+	echo anchor('pegawai/', 'Tidak');
+ ?>
